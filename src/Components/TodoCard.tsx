f22e891c2017b3021abea48157329e3f2b6688f2
Garceller <92178583+GarcelCode@users.noreply.github.com>
2022-02-24 21:34:45 -0600

    Todo Improved

--- src/Components/TodoCard.tsx
+++ src/Components/TodoCard.tsx
@@ -1,14 +1,15 @@
 interface ITodoCardProps{
     text: string;
     done: boolean;
+    action: (e:React.MouseEvent<HTMLSpanElement>)=>void
 }
 
-export function TodoCard ({text, done}:ITodoCardProps) {
+export function TodoCard ({text, done, action}:ITodoCardProps) {
     return <>
-        <div className="flex bg-slate-500 w-80 p-2 gap-3 items-center md:w-96 rounded-md">
-            <input type="checkbox" className="ml-2"/>
-            <p className="grow-[4]">{text}</p>
-            <i className="mr-5">🗑</i>
+        <div className="flex bg-slate-500 w-[22rem] p-2 gap-3 items-center md:w-[26rem] rounded-md">
+            <input type="radio" className="ml-2 min-w-[1.4rem] h-10 rounded-lg hover:scale-110"/>
+            <p className="grow-[1] text-slate-100">{text}</p>
+            <span id={text} onClick={action} title="Eliminar tarea" className="px-2 rounded-sm mr-5 text-xl text-slate-400 hover:bg-slate-700 hover:shadow-lg cursor-pointer duration-300">✖</span>
         </div>
     </>
 }
--- src/Contexts/Auth.tsx
+++ src/Contexts/Auth.tsx
@@ -1,10 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 import {
-    User as FirebaseUser,
+  User as FirebaseUser,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
-  signOut
+  signOut,
 } from "firebase/auth";
 import { auth } from "../firebase-conf";
 
@@ -15,9 +15,8 @@ interface AuthPropsProvider {
 export const authContext = createContext<any>({});
 
 export function AuthProvider({ children }: AuthPropsProvider) {
-
-  const [user, setUser] = useState<FirebaseUser|null>()
-  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState<FirebaseUser | null>();
+  const [loading, setLoading] = useState(true);
 
   const signup = (email: string, password: string) =>
     createUserWithEmailAndPassword(auth, email, password);
@@ -25,16 +24,14 @@ export function AuthProvider({ children }: AuthPropsProvider) {
   const login = (email: string, password: string) =>
     signInWithEmailAndPassword(auth, email, password);
 
-  const logOut = () => {
-    signOut(auth)
-  }
+  const logOut = () => signOut(auth);
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, currentUser => {
-            setUser(currentUser)
-            setLoading(false)
-        });
-    }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <authContext.Provider value={{ signup, login, user, logOut, loading }}>
